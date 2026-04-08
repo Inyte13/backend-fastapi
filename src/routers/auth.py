aec241ef66e590949cd63cac29f0a6b2586a32e8
Inyte13 <luisgardaz13@gmail.com>
2026-04-07 19:11:36 -0500

    chore: router auth, el login

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -6,5 +6,14 @@ from src.core.database import SessionDep
 from src.schemas.usuario import UsuarioCreate, UsuarioRead
 from src.services.auth import registrar_usuario
 
+auth_router = APIRouter(tags=['Auth'], prefix='/auth')
 
 
+@auth_router.post('/login', status_code=201, response_model=UsuarioRead)
+async def login(session: SessionDep, usuario: UsuarioCreate):
+  try:
+    return await registrar_usuario(session, usuario)
+  except IntegrityError:
+    raise HTTPException(
+      status_code=status.HTTP_400_BAD_REQUEST, detail='El username ya existe'
+    )
