75016fdf01d475d1dbff703409e62c30f846ff3d
Inyte13 <luisgardaz13@gmail.com>
2026-04-07 19:10:49 -0500

    chore: modelo usuario, usando uuid4

--- src/models/usuario.py
+++ src/models/usuario.py
@@ -3,3 +3,9 @@ import uuid
 from sqlmodel import Field, SQLModel
 
 
+class Usuario(SQLModel, table=True):
+  id: str = Field(
+    default_factory=lambda: str(uuid.uuid4()), primary_key=True, nullable=False
+  )
+  username: str = Field(unique=True, min_length=3, nullable=False)
+  password: str = Field(min_length=6, nullable=False)
