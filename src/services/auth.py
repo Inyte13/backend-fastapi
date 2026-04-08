3f04abe9291e9be3a21d80b74c3e1956eda8b9f9
Inyte13 <luisgardaz13@gmail.com>
2026-04-07 19:13:19 -0500

    chore: registrar usuario async agregando la pwd hasheada

--- src/services/auth.py
+++ src/services/auth.py
@@ -6,3 +6,9 @@ from src.models.usuario import Usuario
 from src.schemas.usuario import UsuarioCreate
 
 
+async def registrar_usuario(
+  session: Session, usuario: UsuarioCreate
+) -> Usuario:
+  pwd_hashed = await hashear_async(usuario.password)
+  new_usuario = Usuario(username=usuario.username, password=pwd_hashed)
+  return create_usuario(session, new_usuario)
