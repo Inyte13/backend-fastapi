4939b111353fb90e7912a7a6cb80ce14d9d7af47
Inyte13 <luisgardaz13@gmail.com>
2026-04-07 19:15:12 -0500

    chore: orquestadores

--- src/services/usuario.py
+++ src/services/usuario.py
@@ -7,5 +7,13 @@ from src.schemas.usuario import UsuarioUpdate
 
 
 
+def actualizar_usuario(
+  session: Session, id: str, usuario: UsuarioUpdate
+) -> Usuario:
+  usuario_bd = buscar_usuario(session, id)
+  return update_usuario(session, usuario_bd, usuario)
 
 
+def eliminar_usuario(session: Session, id: str) -> None:
+  usuario = buscar_usuario(session, id)
+  delete_usuario(session, usuario)
