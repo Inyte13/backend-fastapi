dabea6702ab80fb99f3dc8a63c64c063d957555a
Inyte13 <luisgardaz13@gmail.com>
2026-04-07 19:09:46 -0500

    chore: hashing async usando solo bcrypt

--- src/core/security.py
+++ src/core/security.py
@@ -5,7 +5,16 @@ import bcrypt
 from src.core.config import settings
 
 
+# encode: lo convierte a bytes
+# decode: lo convierte a str
+def hashear(password: str) -> str:
+  return bcrypt.hashpw(
+    password.encode(), bcrypt.gensalt(rounds=settings.salt_rounds)
+  ).decode()
 
 
+async def hashear_async(password: str) -> str:
+  # Para que no se quede bloqueando el event loop
+  return await asyncio.to_thread(hashear, password)
 
 
