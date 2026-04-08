ff3d2d50f88b81906c30376c883bf05b12ed63be
Inyte13 <luisgardaz13@gmail.com>
2026-04-07 19:10:59 -0500

    chore: usuario schema

--- src/schemas/usuario.py
+++ src/schemas/usuario.py
@@ -1,7 +1,16 @@
 from sqlmodel import SQLModel
 
 
+class UsuarioCreate(SQLModel):
+  username: str
+  password: str
 
 
+class UsuarioRead(SQLModel):
+  id: str
+  username: str
 
 
+class UsuarioUpdate(SQLModel):
+  username: str | None = None
+  password: str | None = None
