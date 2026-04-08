3e5dbac22aed4d5b8142837b4fae514cb5902c85
Inyte13 <luisgardaz13@gmail.com>
2026-04-07 19:12:33 -0500

    style: imports

--- src/routers/usuario.py
+++ src/routers/usuario.py
@@ -5,6 +5,7 @@ from starlette import status
 from src.core.database import SessionDep
 from src.crud.usuario import read_usuarios
 from src.schemas.usuario import UsuarioRead, UsuarioUpdate
+from src.services.usuario import actualizar_usuario, eliminar_usuario
 
 
 
