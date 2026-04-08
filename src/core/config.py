2918daf1263f1b0de9a4c520efabc291163e7721
Inyte13 <luisgardaz13@gmail.com>
2026-04-07 19:08:32 -0500

    chore: settings usando env y default tambien

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,10 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class Settings(BaseSettings):
+  model_config = SettingsConfigDict(env_file='.env')
+  salt_rounds: int = 10
+  port: int = 3000
 
 
+settings = Settings()  # type: ignore
