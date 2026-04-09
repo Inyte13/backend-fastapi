2c155846e7187b75daf71e9417b6970ad39deae7
Inyte13 <luisgardaz13@gmail.com>
2026-04-08 21:56:36 -0500

    chore: pydantic-settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,1 +1,9 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+class Settings(BaseSettings):
+  model_config = SettingsConfigDict(env_file='.env')
+  salt_rounds: int = 10
+  port: int = 3000
+  jwt_secret: str
+  production: bool = False
+  access_token_duration_minutes: int
+settings = Settings()  # type: ignore
