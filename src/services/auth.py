c8b14108288a66e2b00e1802d9d2f10578edf8c8
Inyte13 <luisgardaz13@gmail.com>
2026-04-08 22:02:22 -0500

    chore: loguear y el buscar por username en auth porque en usuario nunca se usa

--- src/services/auth.py
+++ src/services/auth.py
@@ -5,6 +5,11 @@ from src.crud.usuario import create_usuario
 from src.models.usuario import Usuario
 from src.schemas.usuario import UsuarioCreate
 from src.services.usuario import buscar_usuario_by_username
+def buscar_usuario_by_username(session: Session, username: str) -> Usuario:
+  usuario = read_usuario_by_username(session, username)
+  if not usuario:
+    raise ValueError('Credenciales incorrectas')
+  return usuario
 
 
 async def registrar_usuario(
@@ -13,3 +18,8 @@ async def registrar_usuario(
   pwd_hashed = await hashear_async(usuario.password)
   new_usuario = Usuario(username=usuario.username, password=pwd_hashed)
   return create_usuario(session, new_usuario)
+async def loguear_usuario(session: Session, usuario: UsuarioCreate) -> Usuario:
+  usuario_bd = buscar_usuario_by_username(session, usuario.username)
+  if not await verificar_async(usuario.password, usuario_bd.password):
+    raise ValueError('Credenciales incorrectas')
+  return usuario_bd
