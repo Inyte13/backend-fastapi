70a9697370fb6899bc6c5c752d0f9d6e2184fda5
Inyte13 <luisgardaz13@gmail.com>
2026-04-08 22:04:24 -0500

    chore: para todo el usuario_router necesitas estar logueado

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI(
   lifespan=lifespan, title='OAuth App', version='1.0.0', openapi_version='3.0.0'
 )
 
-app.include_router(usuario_router)
+app.include_router(usuario_router, dependencies=[Depends(get_usuario)])
 app.include_router(auth_router)
 
 if __name__ == '__main__':
