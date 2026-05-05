2e67b98faad52a197ae3aa4c7cf3124ba405c381
Inyte13 <luisgardaz13@gmail.com>
2026-05-04 21:25:02 -0500

    feat: encapsulamos los routers en un solo sitio

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,2 +1,6 @@
 from app.api.v1.endpoints import auth, usuarios
 from fastapi import APIRouter
+
+api_router = APIRouter()
+api_router.include_router(auth.router)
+api_router.include_router(usuarios.router)
