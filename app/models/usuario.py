27dfc9fc992337652be5f727054f2fe2cdf0c9e8
Inyte13 <luisgardaz13@gmail.com>
2026-05-04 19:51:47 -0500

    refact: crea el models usuario sqlalchemy puro

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -2,3 +2,10 @@ import uuid
 from app.core.database import Base
 from sqlalchemy import String, Uuid
 from sqlalchemy.orm import Mapped, mapped_column
+class Usuario(Base):
+  __tablename__ = 'usuarios'
+  id: Mapped[uuid.UUID] = mapped_column(
+    Uuid, primary_key=True, default=uuid.uuid4
+  )
+  username: Mapped[str] = mapped_column(String(30), unique=True)
+  password: Mapped[str]
