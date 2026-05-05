710743413bca7fa0fb30db7e0e8658f1a13ffa7f
Inyte13 <luisgardaz13@gmail.com>
2026-05-04 21:25:54 -0500

    feat: database con sqlite, postgres y async

--- app/core/database.py
+++ app/core/database.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+
 from app.core.config import settings
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import (
@@ -7,3 +8,35 @@ from sqlalchemy.ext.asyncio import (
   create_async_engine,
 )
 from sqlalchemy.orm import DeclarativeBase
+
+engine = create_async_engine(
+  settings.DATABASE_URL,
+  echo=True,
+  # future=True # no es necesario para sqlalchemy 2
+)
+
+AsyncSessionLocal = async_sessionmaker(
+  engine,
+  class_=AsyncSession,
+  expire_on_commit=False,
+)
+
+
+class Base(DeclarativeBase):
+  pass
+
+
+async def get_db():
+  """Dependency for database session."""
+  async with AsyncSessionLocal() as session:
+    try:
+      yield session
+      await session.commit()
+    except Exception:
+      await session.rollback()
+      raise
+    finally:
+      await session.close()
+
+
+DbDep = Annotated[AsyncSession, Depends(get_db)]
