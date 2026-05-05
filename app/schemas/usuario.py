213623a7a98f2ad7fb920d47372c8d5c74c6cd10
Inyte13 <luisgardaz13@gmail.com>
2026-05-04 21:09:06 -0500

    chore: el schema del usuario

--- app/schemas/usuario.py
+++ app/schemas/usuario.py
@@ -1,2 +1,20 @@
 import uuid
+
 from pydantic import BaseModel, Field
+
+
+class UsuarioCreate(BaseModel):
+  username: str = Field(min_length=3, max_length=30)
+  password: str = Field(min_length=6)
+
+
+class UsuarioRead(BaseModel):
+  id: uuid.UUID
+  username: str
+  # Solo va en Read porque es lo que respondemos, mandamos al usuario
+  model_config = {'from_attributes': True}
+
+
+class UsuarioUpdate(BaseModel):
+  username: str | None = Field(default=None, min_length=3, max_length=30)
+  password: str | None = Field(default=None, min_length=6)
