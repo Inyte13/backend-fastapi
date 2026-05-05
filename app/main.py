51cd751f804d9ec3cecba7f688a0ccbc6b3ca4d0
Inyte13 <luisgardaz13@gmail.com>
2026-05-04 19:55:50 -0500

    style: imports

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
 from contextlib import asynccontextmanager
 from app.api.v1.router import api_router
+from app.core.config import settings
+from app.core.database import Base, engine
 from fastapi import FastAPI
